727fa4630b317fd5aa633b56ed402008f9c413f1
jim <326695231@qq.com>
2021-05-20 17:22:18 +0800

    链表工具类

---
+++ src/tools/linked_list_tools.py
@@ -0,0 +1,26 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def print_linkedlist(head: ListNode):
+    print("--------分割线------")
+    while head:
+        print(head.val)
+        head = head.next
+
+
+def make_linkedlist(value_list, none=None):
+    if not value_list:
+        return none
+
+    head = ListNode(value_list[0])
+    cur_node = head
+
+    for i in value_list[1:]:
+        node = ListNode(i)
+        cur_node.next = node
+        cur_node = node
+
+    return head
